41f5a447a52f678bb1fa3005ffe21db267758659
RafalSolnica <granadag99@gmail.com>
2023-07-05 11:10:38 +0200

    added theme.js

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  color: {
+    black: "#000",
+    white: "#fff",
+    grey: "hsl(0, 0%, 75%)",
+    teal: "hsl(187, 100%, 25%)",
+    tealHover: "hsl(187, 100%, 33%)",
+    tealActive: "hsl(187, 100%, 40%)",
+    green: "hsl(120, 100%, 25%)",
+    greenHover: "hsl(120, 100%, 35%)",
+    greenActive: "hsl(120, 100%, 45%)",
+    red: "hsl(0, 100%, 50%)",
+    redHover: "hsl(0, 100%, 62%)",
+    redActive: "hsl(0, 100%, 75%)",
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+};
